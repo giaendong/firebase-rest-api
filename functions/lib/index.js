3376b0a729a66c4d5ee8177560f68c16f38b4253
Gia <giaendong@gmail.com>
2018-10-03 14:44:21 +0700

    add crud function and test rest api using postman

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,8 +1,10 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as firebaseHelper from 'firebase-functions-helper';
-import * as express from 'express';
-import * as bodyParser from "body-parser";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const firebaseHelper = require("firebase-functions-helper");
+const express = require("express");
+const bodyParser = require("body-parser");
 const moment = require('moment');
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
@@ -14,40 +16,38 @@ main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
 // webApi is your functions name, and you will pass main as 
 // a parameter
-export const webApi = functions.https.onRequest(main);
-
-
+exports.webApi = functions.https.onRequest(main);
 // Simple CRUD
-
 // Add new dress
 app.post('/dress', (req, res) => {
     const now = moment().unix();
-    const id = `dress-${now}`
+    const id = `dress-${now}`;
     firebaseHelper.firestore
         .createDocumentWithID(db, dressCollection, id, req.body);
     res.send('CREATE SUCCESS');
-})
+});
 // Update new dress
 app.patch('/dress/:dressId', (req, res) => {
     firebaseHelper.firestore
         .updateDocument(db, dressCollection, req.params.dressId, req.body);
     res.send('UPDATE SUCCESS');
-})
+});
 // View a dress
 app.get('/dress/:dressId', (req, res) => {
     firebaseHelper.firestore
         .getDocument(db, dressCollection, req.params.dressId)
         .then(doc => res.status(200).send(doc));
-})
+});
 // View all dress
 app.get('/dress', (req, res) => {
     firebaseHelper.firestore
         .backup(db, dressCollection)
-        .then(data => res.status(200).send(data))
-})
+        .then(data => res.status(200).send(data));
+});
 // Delete a dress 
 app.delete('/dress/:dressId', (req, res) => {
     firebaseHelper.firestore
         .deleteDocument(db, dressCollection, req.params.dressId);
     res.send('DELETE SUCCESS');
-})
+});
+//# sourceMappingURL=index.js.map
